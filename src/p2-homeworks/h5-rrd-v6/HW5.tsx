cda995bb412106f6d01e9dbe658b6545a47c3fe2
gogy9000 <goropashnyj9000@gmail.com>
2022-03-30 13:59:55 +0500

    HW1-completed

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -3,17 +3,18 @@ import Header from './Header'
 import Pages from './Pages'
 import {HashRouter} from "react-router-dom";
 
+
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            <HashRouter>
+           <HashRouter>
 
             <Header/>
 
             <Pages/>
 
-            </HashRouter>
+           </HashRouter>
         </div>
     )
 }
