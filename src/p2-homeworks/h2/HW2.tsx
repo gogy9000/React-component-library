056571eb93433c39f8075c205d5f96367e4054d4
gogy9000 <goropashnyj9000@gmail.com>
2022-04-05 12:45:16 +0500

    HW2 с коментариями для Димы

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -4,14 +4,14 @@ import Affairs from './Affairs'
 
 
 // types
-export type AffairPriorityType = 'High' | 'Low' | 'Middle'// need to fix any(fixed!)
-export type AffairType = { _id: number, name: string, priority: string } // need to fix any(fixed!)
+export type AffairPriorityType = 'High' | 'Low' | 'Middle'
+export type AffairType = { _id: number, name: string, priority: string }
 export type FilterType = 'All' | AffairPriorityType
 
 // constants
 
 
-const defaultAffairs: Array<AffairType> = [ // need to fix any(fixed!)
+const defaultAffairs: Array<AffairType> = [
     {_id: 1, name: 'React', priority: 'High'},
     {_id: 2, name: 'anime', priority: 'Low'},
     {_id: 3, name: 'games', priority: 'Low'},
@@ -20,27 +20,39 @@ const defaultAffairs: Array<AffairType> = [ // need to fix any(fixed!)
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType) => { // need to fix any (fixed!)
+export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType) => {
     if (filter === 'All') return affairs
+
     else return (affairs.filter((affair:AffairType)=>affair.priority===filter))
 }
+// эта функция фильтрует состояние(если фильтр находится в положении "All" то возвращает не филтрованый обЪект affairs,
+// иначе фильтрует методом и возвращает те объекты в массиве у которых свойство priority строго равно значению в хуке filter
 
 
-
-export const deleteAffair = (affairs: Array<AffairType>, _id: number) => { // need to fix any!!!
+export const deleteAffair = (affairs: Array<AffairType>, _id: number) => {
 
     return (affairs.filter((affair: AffairType) => affair._id != _id))
 }
-
+// функция с помощью метода filter(не путать с хуком котрый ниже написан) возвращает только те объекты в массиве defaultAffairs,
+// которые не равны значению _id  которое передается в функцию в качестве аргумена при вызове этой функции в deleteAffairCallback
 
 function HW2() {
-    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any(fixed!)
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs)
+    // это хук в котором сидит defaultAffairs изначально и при нажатии на кнопку delete выкидывает один из обЪектов,
+    // который в нем находится
     const [filter, setFilter] = useState<any>('All')
+    //этот хук держит в себе значение по которому филЬтруется defaultAffairs
 
     const filteredAffairs = filterAffairs(affairs, filter)
+    //переменная с отфильтрованным массивом defaultAffairs в качестве значения
+    // filterAffairs фильтруется по массиву который сидит в Хуке affairs,
+    // а не тот котрый в самом верху написан, то есть после удаления какого либо обЪекта массив в хуке affairs
+    // будет фильтроваться с учетом того что из него обьекты могут быть удалены посредством функции deleteAffair
 
-    const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id)) // need to fix any(fixed!)
-
+    const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id))
+    // вот здесь вызывается deleteAffair и вернувшееся значение сразу же залетает в хук setAffairs
+    // deleteAffairCallback это колбек который вызывается в компоненте Affair(не путать с Affairs ) и при вызове в
+    // качестве аргумента указывается _id того обЪекта который нужно удалить
 
 
     return (
