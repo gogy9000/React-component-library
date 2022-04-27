74c77e7df6b72d5504fa015ca929907ed660bc48
gogy9000 <goropashnyj9000@gmail.com>
2022-04-27 21:39:23 +0500

    Updates

---
+++ precache-manifest.7d64594b568409504d4c7236e29ab2bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441f54170e27b567fb9f0daa705e9d99",
+    "url": "/simple-homework-by-Ignat-Zakalinsky/index.html"
+  },
+  {
+    "revision": "061929ef30234c7d993d",
+    "url": "/simple-homework-by-Ignat-Zakalinsky/static/css/main.276a2fc4.chunk.css"
+  },
+  {
+    "revision": "d3c516966ab2f67985d3",
+    "url": "/simple-homework-by-Ignat-Zakalinsky/static/js/2.9d3dad2a.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/simple-homework-by-Ignat-Zakalinsky/static/js/2.9d3dad2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061929ef30234c7d993d",
+    "url": "/simple-homework-by-Ignat-Zakalinsky/static/js/main.d366cda4.chunk.js"
+  },
+  {
+    "revision": "c214f23f543f60ed83f3",
+    "url": "/simple-homework-by-Ignat-Zakalinsky/static/js/runtime-main.d4636143.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6133f20483b59b8b00d71b99109f9701.js"
+  "/simple-homework-by-Ignat-Zakalinsky/precache-manifest.7d64594b568409504d4c7236e29ab2bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-homework-by-Ignat-Zakalinsky/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
