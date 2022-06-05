2e4e3a7ea9192edf667a718f17f3b07c4a33611c
gogy9000 <goropashnyj9000@gmail.com>
2022-06-05 20:11:26 +0500

    HW11 completed

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -4,8 +4,8 @@ import 'rc-slider/assets/index.css';
 
 
 type SuperDoubleRangePropsType = {
-    setValue1:  React.Dispatch<React.SetStateAction<number>>
-    setValue2:  React.Dispatch<React.SetStateAction<number>>
+    setValue1: React.Dispatch<React.SetStateAction<number>>
+    setValue2: React.Dispatch<React.SetStateAction<number>>
     value?: number[]
     min?: number
     max?: number
@@ -14,9 +14,9 @@ type SuperDoubleRangePropsType = {
 }
 
 export const SuperDoubleRange = (props: SuperDoubleRangePropsType) => {
-    let {value, min, max, step, setValue1,setValue2} = props
+    let {value, min, max, step, setValue1, setValue2} = props
 
-    const onChangeCallback = (val:any) => {
+    const onChangeCallback = (val: any) => {
 
         setValue1(val[0] as number)
         setValue2(val[1] as number)
@@ -25,9 +25,15 @@ export const SuperDoubleRange = (props: SuperDoubleRangePropsType) => {
 
     return (
 
-        <div style={{width: 200, margin:10}}>
+        <div style={{width: 130, margin: 10}}>
 
             <Slider range
+                    handleStyle={{
+                        backgroundColor: "rgb(0 117 255)", height: 14, opacity: 1,
+                        border: "none", marginTop: -3, boxShadow: "none"
+                    }}
+                    trackStyle={{border: "1px solid rgb(59 117 186)", backgroundColor: "rgb(0 117 255)", height: 8}}
+                    railStyle={{border: "1px solid darkGray", height: 8}}
                     onChange={onChangeCallback}
                     value={value}
                     min={min}
